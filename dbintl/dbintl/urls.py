982c92eeb1c3c5fe4b9fcab6d9e21c7377ccee38
gchoo <zhugino@tmail.com>
2015-07-14 16:21:30 +0800

    add hello url

--- dbintl/dbintl/urls.py
+++ dbintl/dbintl/urls.py
@@ -15,7 +15,19 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from dbcr.views import *
 
 urlpatterns = [
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^mysite/', include('mysite.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', 'dbcr.views.hello', name='hello'),
 ]
